80a4f593037cba297cf736a6f31ad952cbb8af5d
Piyush Mahamuni <mahamunipiyush@outlook.com>
2024-06-10 11:00:30 +0530

    Checkpoint: Add rsp.launch.py

--- src/diff_bot/launch/rsp.launch.py
+++ src/diff_bot/launch/rsp.launch.py
@@ -1,36 +1,32 @@
-import xacro
 import os
-from launch import LaunchDescription, LaunchContext
-from launch_ros.actions import Node
+import xacro
+from launch import LaunchDescription
 from ament_index_python import get_package_share_directory
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
-    use_sim_time = LaunchConfiguration("use_sim_time", default=True)
     pkg_share = get_package_share_directory("diff_bot")
-    robot_description_file = os.path.join(pkg_share, "urdf", "gazebo_sim.xacro")
-    robot_description = xacro.process_file(robot_description_file).toxml()
-    robot_description = LaunchConfiguration("robot_description", default=robot_description)
+    urdf_file = os.path.join(pkg_share, "urdf", "robot.urdf.xacro")
+    robot_description = xacro.process_file(urdf_file).toxml()
+    use_sim_time = LaunchConfiguration("use_sim_time", default=True)
     params = {"robot_description": robot_description, "use_sim_time": use_sim_time}
     rsp = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="screen",
-        parameters=[params],
+        parameters=params.items(),
     )
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            "use_sim_time",
-            default_value="true",
-            description="use sim time if true",
-        ),
-        DeclareLaunchArgument(
-            "robot_description",
-            default_value=robot_description,
-            description="point to either ros2_control_sim.xacro or gazebo_sim.xacro",
-        ),
-        rsp
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                use_sim_time,
+                description="use gazebo clock if set to true",
+            ),
+            rsp
+        ]
+    )
